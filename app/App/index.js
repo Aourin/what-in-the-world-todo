5c3d5c118374bb786cd9d19827dcb1c118981646
Aourin <TheTitan8706@gmail.com>
2016-09-22 08:48:35 -0400

    feat(webpack): initial webpack config with app base

---
+++ app/App/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+/**
+ * Base App Class for loading in app components
+ * @class App
+ */
+export default class App extends Component {
+  constructor () {
+    super();
+  }
+
+  render () {
+    return (
+      <h3>Super Cool Cheese Fish!</h3>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+//  Still deciding if I like pre comma lines to make it look 'cleaner' in commits and styling for this
+var path              = require('path')
+  , webpack           = require('webpack')
+  , HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: {
+    app: './app/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  plugins: [
+    //  Handles HTML File build
+    new HtmlWebpackPlugin({
+      hash: true,
+      filename: 'index.html',
+      template:  path.resolve(__dirname, 'app') + '/index.html',
+    }),
+    //  Make it Ugly and Stuff
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+      output: {
+        comments: false,
+      }
+    })
+  ]
+};
