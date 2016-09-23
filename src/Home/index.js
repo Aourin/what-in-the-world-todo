0529a5c070bc6d479fb0e4ba20f1a09f304ae9dd
Aourin <harrymho@gmail.com>
2016-09-23 09:37:16 -0400

    feat(TodoList): refactors routes and adds todolist base view

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 export default class Home extends Component {
   constructor () {
@@ -9,7 +10,13 @@ export default class Home extends Component {
     return (
       <div>
         <h1>Home Route</h1>
+        <Link to='/todos'>Go To TodoList</Link>
       </div>
     );
   }
 }
+
+
+//TODO: Add Basic App Layout
+//TODO: Add Header
+//TODO: Add Footer
