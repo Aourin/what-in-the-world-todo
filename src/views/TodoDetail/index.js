e294cc93b52044a864adb9c620178c0f8d25688d
Aourin <harrymho@gmail.com>
2016-09-26 01:56:04 -0400

    feat(TodoDetail): adds editable title with buffer state

--- src/views/TodoDetail/index.js
+++ src/views/TodoDetail/index.js
@@ -5,21 +5,101 @@ export default class TodoDetail extends Component {
     params: T.object
   };
   state = {
-    todo: {}
+    todo: {},
+    buffer: {},
+    editTitle: false
   };
   constructor () {
     super();
   }
-  componentWillReceiveProps () {
-    console.log('should rec');
+
+  /**
+   * Handles hitting enter on the input
+   * @param e
+   * @param prop
+   */
+  handleKeyDown (e, prop) {
+    if (e.keyCode === 13) {
+      this.onSetProp(prop, e.target.value);
+    } else if (e.keyCode === 27) {
+      this.onResetBuffer();
+    }
   }
+
+  /**
+   * Handles buffer changes on the state model
+   * @param prop
+   * @param value
+   */
+  onChangeProp (prop, value) {
+    const updatedBuffer = {...this.state.todo, [prop]: value};
+    this.setState({
+      buffer: updatedBuffer
+    });
+  }
+
+  /**
+   * Sets a prop on the state model
+   * @param prop
+   * @param value
+   */
+  onSetProp (prop, value) {
+    const updated = {...this.state.todo, [prop]: value};
+
+    this.setState({
+      todo: updated,
+      buffer: updated,
+      editTitle: false
+    });
+  }
+
+  /**
+   * Resets the  buffer
+   */
+  onResetBuffer () {
+    this.setState({
+      buffer: {...this.state.todo},
+      editTitle: false
+    });
+  }
+
+  /**
+   * Renders the title and input
+   * @returns {JSX}
+   */
+  renderTitle () {
+    console.log('title', this.state.todo);
+    const { editTitle, todo, buffer } = this.state;
+    if (editTitle) {
+      return (
+        <input
+          name='title'
+          placeholder='Enter Title of Cool Todo List'
+          className='form-control form-control-lg input input--line-only'
+          value={buffer.title || ''}
+          onKeyDown={(e) => {this.handleKeyDown(e, 'title');}}
+          onChange={(e) => {this.onChangeProp('title', e.target.value)}}
+          onBlur={(e) => {this.onSetProp('title', e.target.value);}} />
+      );
+    } else {
+      const title = todo.title ? todo.title : 'Please Add A Title';
+      return (
+        <div className='panel-title'>
+          <i className='fa fa-pencil-square-o' onClick={() => {this.setState({editTitle: true})}}></i> {title}
+        </div>
+      );
+    }
+
+  }
+
   render () {
     const { params } = this.props;
+
     return (
-      <div>
+      <div className='detail-pane'>
         <div className='row'>
           <div className='col-md'>
-            <h3>Fancy Detail</h3>
+            {this.renderTitle()}
             <p>Details for {params.id}</p>
           </div>
         </div>
@@ -27,3 +107,5 @@ export default class TodoDetail extends Component {
     );
   }
 }
+
+//TODO: Refactor Input to separate component for handling key bindings
