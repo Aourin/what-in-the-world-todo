ac4a457e4c285668baafc7f60f91459e27e2e151
Aourin <harrymho@gmail.com>
2016-09-25 22:27:51 -0400

    feat(router): sets up view splitting for todolist

--- src/router.js
+++ src/router.js
@@ -1,28 +1,81 @@
-import React    from 'react';
-import Home     from './views/Home';
-import TodoList from './views/TodoList';
-
+//  Vendor
+import React              from 'react';
 import {
   BrowserRouter as Router,
-  Match
+  Match,
+  Redirect
 } from 'react-router';
+
+//  Views
+import Home               from './views/Home';
+import TodoList           from './views/TodoList';
+
+//  Components
+import FlexRouteContainer from './components/FlexRouteContainer';
+
+
+
+const SPLIT_VIEW  = 'SPLIT_VIEW';
+const SINGLE_VIEW = 'SINGLE_VIEW';
 
 //  Match with subroutes
 const MatchWithSubRoutes = (route) => (
-  <Match {...route} render={(props) => (
-    <route.component {...props} routes={route.routes}/>
-  )}/>
+  <Match {...route} render={(props) => {
+    let renderItem;
+    switch (route.type) {
+      case SPLIT_VIEW:
+
+        //  Sends props down to component panels
+        const panels = route.panels.map((panel) => {
+
+          //  Checks for component and returns a copy of the panel with props added
+          if (panel.component) {
+            return {
+              ...panel,
+              props: { ... props }
+            };
+          } else {
+            return panel;
+          }
+        });
+
+        renderItem = <FlexRouteContainer panels={panels}/>;
+        break;
+      case SINGLE_VIEW:
+        renderItem = <route.component {...props} routes={route.routes}/>;
+        break;
+      default:
+        renderItem = <route.component {...props} routes={route.routes}/>;
+    }
+
+    return renderItem;
+  }}/>
 );
 
 // Route Configuration
 const routes = [
   {
     pattern: '/',
-    component: Home
+    component: (props) => (
+      <Redirect to={{
+        pathname: '/todos',
+        state: { from: props.location }
+      }}/>
+    )
   },
   {
     pattern: '/todos',
-    component: TodoList
+    type: SPLIT_VIEW,
+    panels: [
+      {
+        size: 1,
+        component: TodoList
+      },
+      {
+        size: 2,
+        children: <h3>Empty Detail View</h3>
+      }
+    ]
   }
 ];
 
